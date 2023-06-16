89f0a4dd838ee4dd880a1161404e9e62d60aac49
Ryan Chomistek <ryan.chomistek@gmail.com>
2023-06-15 22:58:52 -0700

    setup

---
+++ stickypad/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>+</button>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          hello world
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
