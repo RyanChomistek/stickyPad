742c2488fb14a069d1622e2a0bfe728f6b1f874b
Ryan Chomistek <ryan.chomistek@gmail.com>
2023-06-17 21:44:15 -0700

    adding context menu

--- stickypad/src/NotePad.tsx
+++ stickypad/src/NotePad.tsx
@@ -2,6 +2,9 @@ import React, { Component, useState, useEffect, KeyboardEvent } from 'react';
 import { Note, NoteComponent, MousePosition } from './Note';
 import { Button, IconButton } from "@mui/material";
 import { Add } from '@mui/icons-material';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import './Note.css';
 
 export const NotePad: React.FC = () => {
@@ -23,12 +26,36 @@ export const NotePad: React.FC = () => {
     };
   }, []);
 
-  var makeNote = () => {
-    setNotes(prevNotes => [...prevNotes, new Note("title", "body", mousePos)]);
+  const [contextMenu, setContextMenu] = React.useState<{
+    mouseX: number;
+    mouseY: number;
+  } | null>(null);
+  const handleContextMenu = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setContextMenu(
+      contextMenu === null
+        ? {
+            mouseX: event.clientX + 2,
+            mouseY: event.clientY - 6,
+          }
+        : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+          // Other native context menus might behave different.
+          // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+          null,
+    );
   };
 
+  const closeContextMenu = () => {
+    setContextMenu(null);
+  };
+
+  var makeNote = () => {
+    setNotes(prevNotes => [...prevNotes, new Note("title", "body", mousePos)]);
+    closeContextMenu();
+  };
+  
   return (
-    <div className="NotePad" tabIndex={0}>
+    <div className="NotePad" onContextMenu={handleContextMenu}>
       <IconButton
         onClick={() => makeNote()}>
         <Add/>
@@ -36,7 +63,18 @@ export const NotePad: React.FC = () => {
       {notes.map((element, index) => {
         return NoteComponent(element, index);
       })}
-
+      <Menu
+        open={contextMenu !== null}
+        onClose={closeContextMenu}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu !== null
+            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+            : undefined
+        }
+      >
+        <MenuItem onClick={makeNote}>Add Note</MenuItem>
+      </Menu>
     </div>
   )
 }
